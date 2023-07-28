57f7e41261efd12b6d09a8f636b4db66e0ff18e0
youssra <youssraanene20@gmail.com>
2023-07-28 10:38:42 -0700

    Integers division with debug

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
+
