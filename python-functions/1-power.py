00cbc9ca89f3abcfcfe60fbe1f9af557cad1c189
youssra <youssraanene20@gmail.com>
2023-07-28 09:40:11 -0700

    power

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 def pow(a, b):
-    return a ^ b
+    # Initialize the result to 1
+    result = 1
+
+    # Handle the special case when b is 0 (a^0 = 1)
+    if b == 0:
+        return result
+
+    # If b is negative, convert a^b to 1/(a^(-b))
+    if b < 0:
+        a = 1 / a
+        b = -b
+
+    # Perform the power calculation using a loop
+    while b > 0:
+        if b % 2 == 1:
+            result *= a
+        a *= a
+        b //= 2
+
+    return result
+
