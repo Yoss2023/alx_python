c484e55522f89095b999c38fbe61cf742474b50a
youssra <youssraanene20@gmail.com>
2023-07-28 10:20:36 -0700

    Import a simple function from a simple file

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# Import the add function from add_0.py
+from add_0 import add
+
+# Assign values to variables a and b
+a = 1
+b = 2
+
+# Calculate the result of the addition using the add function
+result = add(a, b)
+
+# Print the result in the desired format
+print(f"{a} + {b} = {result}")
+
