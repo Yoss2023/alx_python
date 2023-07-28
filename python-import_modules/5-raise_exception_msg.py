827da5d5f525244de1aa561abcbeb8b2697c5447
youssra <youssraanene20@gmail.com>
2023-07-28 10:52:04 -0700

    Raise a message

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -2,4 +2,11 @@
 if __name__ == "__main__":
     def raise_exception_msg(message=""):
         raise NameError(message)
+#!/usr/bin/python3
+raise_exception_msg = __import__('5-raise_exception_msg').raise_exception_msg
+
+try:
+    raise_exception_msg("C is fun")
+except NameError as ne:
+    print(ne)
 
