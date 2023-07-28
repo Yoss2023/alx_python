913e9d118d2c5560a727081fc71f5f13ab54915e
youssra <youssraanene20@gmail.com>
2023-07-28 10:47:49 -0700

    Raise exception

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -2,4 +2,11 @@
 if __name__ == "__main__":
     def raise_exception():
         raise TypeError("Exception has been raised")
+raise_exception = __import__('4-raise_exception').raise_exception
+
+try:
+    raise_exception()
+except TypeError as te:
+    print(te)
+
 
