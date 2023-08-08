2c336ee8fc9d66ba04bed92b8f78ee078bf24ca1
youssra <youssraanene20@gmail.com>
2023-08-08 10:25:29 -0700

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square's sides (private).
+    """
+
     def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square's sides.
+        """
         self.__size = size
 
