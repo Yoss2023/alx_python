1638ece9790afaa451d4270a6874ff9fa1d66ab0
youssra <youssraanene20@gmail.com>
2023-09-23 08:45:00 -0700

    0-square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a new square.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
 
-    def area(self):
-        return self.__size ** 2
