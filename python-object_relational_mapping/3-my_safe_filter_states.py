ac8909838732f3c9fd717b7e19b66793bbf3bded
youssra <youssraanene20@gmail.com>
2023-09-30 08:53:01 -0700

    SQL Injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+        # Create a cursor object
+        cursor = db.cursor()
+
+        # Execute the safe query using parameterized query
+        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+        cursor.execute(query, (state_name,))
+
+        # Fetch all the rows
+        rows = cursor.fetchall()
+
+        # Display the results
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+
+    finally:
+        # Close the cursor and database connection
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
+
