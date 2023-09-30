28e48338adccab661ca5d5c4d5c26542b69fa5fd
youssra <youssraanene20@gmail.com>
2023-09-30 08:56:40 -0700

    All cities by state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+        # Create a cursor object
+        cursor = db.cursor()
+
+        # Execute the query to get cities of the specified state
+        query = """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """
+        cursor.execute(query, (state_name,))
+
+        # Fetch all the rows
+        rows = cursor.fetchall()
+
+        # Display the results
+        if rows:
+            cities = ', '.join(row[0] for row in rows)
+            print(cities)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+
+    finally:
+        # Close the cursor and database connection
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
+
