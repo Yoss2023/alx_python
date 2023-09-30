16d651f3d882abb00b19d2048df65c435b07ec1c
youssra <youssraanene20@gmail.com>
2023-09-30 08:25:43 -0700

    Hello Flask!

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,11 +1,41 @@
+"""
+Module: 0-hello_route
+
+Description:
+    This module contains a simple Flask web application with a single route.
+    The application listens on 0.0.0.0 at port 5000.
+
+Routes:
+    - /: Displays "Hello HBNB!" when accessed.
+
+Usage:
+    - Run this script to start the Flask application.
+    - Access the / route in a web browser or using curl to see the response.
+
+Example:
+    Terminal 1:
+        $ python3 0-hello_route.py
+        * Running on http://0.0.0.0:5000/ (Press CTRL+C to quit)
+    
+    Terminal 2:
+        $ curl 0.0.0.0:5000 ; echo "" | cat -e
+        Hello HBNB!$
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Route handler for the / route.
+
+    Returns:
+        str: "Hello HBNB!"
+    """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
 
